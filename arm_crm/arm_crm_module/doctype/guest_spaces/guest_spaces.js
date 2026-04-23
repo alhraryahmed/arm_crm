40633a1f9f634361d3d9eaa6ca2d3cca0fc1866e
ahmed alhrary <alhraryahmed110@gmail.com>
2026-04-23 17:53:29 +0000

    تحديث 23/4

---
+++ arm_crm/arm_crm_module/doctype/guest_spaces/guest_spaces.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, alhrary and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("guest_spaces", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- arm_crm/hooks.py
+++ arm_crm/hooks.py
@@ -6,9 +6,11 @@ app_email = "alhraryahmed110@gmail.com"
 app_license = "mit"
 
 fixtures = [
-    {"dt": "Client Script", "filters": [["module", "=", "arm_crm"]]},
-    {"dt": "Print Format", "filters": [["module", "=", "arm_crm"]]}
+    {"dt": "Client Script", "filters": [["module", "=", "arm_crm_module"]]},
+    {"dt": "Print Format", "filters": [["module", "=", "arm_crm_module"]]},
+    {"dt": "Custom HTML Block", "filters": [["module", "=", "arm_crm_module"]]}
 ]
+
 
 # Apps
 # ------------------
